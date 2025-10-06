5269702bc16fd30631a726b8dbe2b9a28563198d
Alba Lopez <alba.lopez@in2.es>
2025-10-06 13:47:39 +0200

    fix tests

--- src/app/shared/forms/status-selector/status-selector.component.spec.ts
+++ src/app/shared/forms/status-selector/status-selector.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { beforeEach, describe, expect, it } from '@jest/globals';
 
 import { StatusSelectorComponent } from './status-selector.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('StatusSelectorComponent', () => {
   let component: StatusSelectorComponent;
@@ -9,7 +10,7 @@ describe('StatusSelectorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StatusSelectorComponent]
+      imports: [StatusSelectorComponent, TranslateModule.forRoot()],
     })
     .compileComponents();
     
