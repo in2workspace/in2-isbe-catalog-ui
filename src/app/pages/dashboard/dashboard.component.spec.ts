f3fe4d766b9874c8139c0e19ee77d553da980813
Alba Lopez <alba.lopez@in2.es>
2025-11-05 16:46:17 +0100

    testing

--- src/app/pages/dashboard/dashboard.component.spec.ts
+++ src/app/pages/dashboard/dashboard.component.spec.ts
@@ -1,21 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { beforeEach, describe, expect, it } from '@jest/globals';
 
-import { DashboardComponent } from './dashboard.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MarkdownModule } from 'ngx-markdown';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { AuthService } from 'src/app/guard/auth.service';
+import { authServiceMock, oidcSecurityServiceMock } from 'src/testing/mocks/oidc-security.service.mock';
+import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let activatedRouteMock: any;
 
   beforeEach(async () => {
+    activatedRouteMock = {
+      snapshot: {
+        paramMap: {
+          get: (key: string) => key === 'keywords' ? 'example' : null
+        }
+      }
+    };
+
     await TestBed.configureTestingModule({
-    providers: [
-      { provide: ActivatedRoute, useValue: { snapshot: { queryParamMap: convertToParamMap({}) } } }
-    ],
-      imports: [DashboardComponent, HttpClientTestingModule, TranslateModule.forRoot()]
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteMock },  
+        { provide: AuthService, useValue: authServiceMock }, 
+        { provide: OidcSecurityService, useValue: oidcSecurityServiceMock },
+      ],
+      imports: [TranslateModule.forRoot(),HttpClientTestingModule,  MarkdownModule.forRoot()] 
     })
     .compileComponents();
     
@@ -27,4 +42,5 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
