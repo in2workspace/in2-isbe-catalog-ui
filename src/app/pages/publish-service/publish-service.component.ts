58bfe0b4a826cdcc22a4602abe65c5c67b566bbd
maxime-llos <122364734+maxime-llos@users.noreply.github.com>
2026-05-15 11:12:37 +0200

    llos v.4.3 (#37)

--- src/app/pages/publish-service/publish-service.component.ts
+++ src/app/pages/publish-service/publish-service.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { IsbeBannerComponent } from 'src/app/shared/isbe-banner/isbe-banner.component';
+import { MenuStateService } from 'src/app/services/menu-state.service';
 
 @Component({
   selector: 'app-publish-service',
@@ -10,5 +12,10 @@ import { IsbeBannerComponent } from 'src/app/shared/isbe-banner/isbe-banner.comp
   styleUrl: './publish-service.component.css'
 })
 export class PublishServiceComponent {
+  constructor(private router: Router, private menuState: MenuStateService) {}
 
+  goToProductSpec() {
+    this.menuState.setActiveTab('offerings', 'productspec');
+    this.router.navigate(['/my-offerings']);
+  }
 }
